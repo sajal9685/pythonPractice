bed1cd00bfc512ca10a1c64eacdedfa2648b75b6
srajal tiwari <tiwarisrajal880@gmail.com>
2025-03-29 00:46:16 +0530

    star pattern

---
+++ star_pattern.py
@@ -0,0 +1,18 @@
+print(" pattern 1")
+
+n=int(input("enter number: "))
+for i in range(1,n+1):
+    print(" "*(n-i), end="")
+    print("*"*(2*i-1),end="")
+    print(" ")
+
+print(" pattern 2")
+
+for i in range(1,n+1):
+    if(i==1 or i==n):
+        print("*"* n, end="")
+    else :
+        print("*", end="")
+        print(" "* (n-2),end="")
+        print("*",end="")
+    print("")    
