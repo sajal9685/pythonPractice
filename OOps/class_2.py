5af7f6d851f5f18f2893e791aa2f82e171b11705
sajal chaturvedi <sajalchaturvedi8@gmail.com>
2025-04-20 00:35:37 +0530

    oops-class

---
+++ OOps/class_2.py
@@ -0,0 +1,16 @@
+class calculator():
+    def __init__(self,n):
+        self.n=n      
+    def square(self):
+       print(self.n**2)
+    def cube (self):
+        print (self.n**3)
+    def sqRoot(self):
+        print( self.n*0.5)
+        
+    
+num=calculator(4)
+num.square()
+num.cube()
+num.sqRoot()
+    
---
+++ leetcode/Q_1.py
@@ -0,0 +1,24 @@
+nums=[2,12,7,15]
+target = 9
+i=0
+
+while i<=3:
+    j=i+1 
+    while j<=3: 
+        if nums[i]+nums[j]==target:
+            print([i,j])
+        j+=1
+    i+=1     
+
+
+            
+
+
+
+
+
+
+
+
+    
+
